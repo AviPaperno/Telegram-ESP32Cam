06d002cff1078215a09f6fb17bb0725da463ed2e
AviPaperno <vpap1997@mail.ru>
2023-04-29 16:05:07 +0300

    Create config.py

---
+++ TELEGRAM_CODE/config.py
@@ -0,0 +1,12 @@
+# MQTT credentials
+MQTT_HOST = ""
+MQTT_PORT = 1883
+MQTT_KEEPALIVE = 60
+MQTT_LOGIN = ""
+MQTT_PASSWORD = ""
+
+# Telegram credentials
+TELEGRAM_TOKEN = ""
+
+# Configuration
+TIME_WAIT_PHOTO = 30
